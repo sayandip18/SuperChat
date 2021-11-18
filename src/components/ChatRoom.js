44e5bed25065441d13523a730b57fd3fcdb382d2
Sayandip Halder <sayandiph4@gmail.com>
2021-11-18 18:02:25 +0530

    add sign-out functionality

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import SignOut from './SignOut';
 
 function ChatRoom() {
     return (
         <div>
+            <SignOut />
             Chat
         </div>
     )
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Button from '@mui/material/Button';
+import { auth } from '../firebase.js';
 
 function SignOut() {
     return (
-        <div>
-            
+        <div style={{
+            display: 'flex', justifyContent: 'center', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, borderBottom: 'solid 1px lightgray', zIndex: '10'
+        }}>
+            <Button style={{ padding: '20px', fontSize: '15px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()}>Sign Out</Button>
         </div>
     )
 }
